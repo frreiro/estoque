f74c755cc64792192f5e58b9b44bcff42ff5fb46
Lucas Viana <lucas.viana@watch.tv.br>
2025-06-11 23:37:38 -0300

    feat: update rules

--- entrada/views.py
+++ entrada/views.py
@@ -45,10 +45,10 @@ def update_entrada(request, pk):
             form.save(commit=False)
             if form.cleaned_data['produto'].quantidade - quantidade + form.cleaned_data['quantidade'] <= 0:
                 form.add_error('quantidade', 'Quantidade insuficiente em estoque.')
-                return render(request, template_name, {'form': form})
+                return render(request, template_name, {'form': form, 'pk': pk})
             if form.cleaned_data['quantidade'] <= 0:
                 form.add_error('quantidade', 'Insira apenas valores positivos maiores que zero.')
-                return render(request,template_name, {'form': form})
+                return render(request,template_name, {'form': form, 'pk': pk})
             form.cleaned_data['produto'].quantidade = form.cleaned_data['produto'].quantidade - quantidade + form.cleaned_data['quantidade']
             form.cleaned_data['produto'].save_base()
             form.save()
--- saida/views.py
+++ saida/views.py
@@ -42,12 +42,12 @@ def update_saida(request, pk):
         form = SaidaForm(request.POST,instance=saida)
         if form.is_valid():
             form.save(commit=False)
-            if form.cleaned_data['quantidade'] > form.cleaned_data['produto'].quantidade:
+            if form.cleaned_data['quantidade'] > form.cleaned_data['produto'].quantidade + quantidade:
                 form.add_error('quantidade', 'Quantidade insuficiente em estoque.')
-                return render(request, template_name, {'form': form})
+                return render(request, template_name, {'form': form, 'pk': pk})
             if form.cleaned_data['quantidade'] <= 0:
                 form.add_error('quantidade', 'Insira apenas valores positivos maiores que zero.')
-                return render(request, template_name, {'form': form})
+                return render(request, template_name, {'form': form, 'pk': pk})
             form.cleaned_data['produto'].quantidade = form.cleaned_data['produto'].quantidade + quantidade - form.cleaned_data['quantidade']
             form.cleaned_data['produto'].save_base()
             form.save()
